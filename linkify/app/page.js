88c436b7f2695a58e98463fa27c0a59d71fbcf0a
Manasi V Kathale <mansikathale5@gmail.com>
2025-10-27 23:10:27 +0530

    Added some Ui changes in page.js

--- linkify/app/page.js
+++ linkify/app/page.js
@@ -3,7 +3,21 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <section className="grid grid-cols-2"></section>
+      <section className="grid grid-cols-2 h-[50vh]">
+        <div className="bg-blue-50 flex flex-col gap-4">
+          <p className="text-3xl font-bold">
+            One short link, Infinite possibilities.
+          </p>
+          <p>The simplest way to shorten anything — Linkify.</p>
+        </div>
+        <div className="bg-gray-800 flex jusify-start relative">
+          <Image
+            src={"/vector.jpg"}
+            alt="A URL professional Image"
+            fill={true}
+          />
+        </div>
+      </section>
     </main>
   );
 }
