6e2a66742e64b7b9b541b21faab39e0751cf8ca1
Manasi V Kathale <mansikathale5@gmail.com>
2025-11-13 18:00:38 +0530

    Shorten Page Has been Created

--- linkify/app/shorten/page.js
+++ linkify/app/shorten/page.js
@@ -1,11 +1,39 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 
 const Shorten = () => {
+  const [url, seturl] = useState("");
+  const [shorturl, setshorturl] = useState("");
+  const [generated, setGenerated] = useState(false);
+
   return (
-    <div>
-      <h1>Simplify your sharing — Linkify makes it easy.</h1>
-      <div>
-        <input type="text" placeholder="Enter / Paste Your URL" />
+    <div className="mx-auto max-w-lg bg-gray-300 flex flex-col my-16 p-8 rounded-lg gap-6 ">
+      <h1 className="font-bold text-black text-xl ">
+        Simplify your sharing — Linkify makes it easy.
+      </h1>
+      <div className="flex flex-col gap-2 ">
+        <input
+          type="text"
+          value={url}
+          className="px-4 py-4 text-black focus:outline-gray-700 rounded-md"
+          placeholder="Enter / Paste Your URL"
+          onChange={(e) => {
+            seturl(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          value={shorturl}
+          placeholder="Paste Your YouTube Video Link Here"
+          className="px-4 py-4  text-black focus:outline-gray-700 rounded-md"
+          onChange={(e) => {
+            setshorturl(e.target.value);
+          }}
+        />
+        <button className="bg-gray-800 my-4 shadow-lg text-white rounded-lg p-3">
+          Create
+        </button>
       </div>
     </div>
   );
